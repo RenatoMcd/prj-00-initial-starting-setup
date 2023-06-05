305b48196f931c1807c68cf8e73d727a14983650
Renato Macedo <renatomcd.rm@gmail.com>
2023-06-05 13:50:06 +0100

    implemented autobind decorator and query for user input

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,62 @@
+// autobin decorator
+function autobind(
+    _: any,
+    _2: string,
+    descriptor: PropertyDescriptor
+) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   form: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
-      "project-input"
+      'project-input'
     )! as HTMLTemplateElement;
-    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
     const importedNode = document.importNode(
       this.templateElement.content,
       true
     );
     this.form = importedNode.firstElementChild as HTMLFormElement;
+    this.form.id = 'user-input';
+
+    this.titleInputElement = this.form.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.form.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.form.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  @autobind
+  private submitHandler(e: Event) {
+    e.preventDefault();
+
+  } 
+
+  private configure() {
+    this.form.addEventListener('submit', this.submitHandler);
+  }
+
   private attach() {
-    this.hostElement.insertAdjacentElement("afterbegin", this.form);
+    this.hostElement.insertAdjacentElement('afterbegin', this.form);
   }
 }
 
